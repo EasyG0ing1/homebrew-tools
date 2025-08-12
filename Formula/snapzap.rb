51e3cb92db9447b84cd1821646972bc2057ef51a
Michael Sims <sims.mike@gmail.com>
2025-08-12 01:02:31 -0700

    initial commit with snapzap

--- Formula/snapzap.rb
+++ Formula/snapzap.rb
@@ -1,17 +1,17 @@
 class Snapzap < Formula
   desc "SnapZap helps you clean up snapshots on APFS volumes in MacOS"
   homepage "https://github.com/EasyG0ing1/SnapZap"
-  version "1.0.0"
+  version "1.0.5"
 
   on_macos do
     on_intel do
       url "https://github.com/EasyG0ing1/SnapZap/releases/download/1.0.0/snapzap-1.0.0-macos-x64.tar.gz"
-      sha256 "3164436de6dffceae5245d6e72e2010d022a5e931aa6d23e9ff45563810e64e6"
+      sha256 "22b3a8d4fdb0af6c377fe1c76017aac14c9d0672e8123fcdf67dea690a1f7214"
     end
 
     on_arm do
       url "https://github.com/EasyG0ing1/SnapZap/releases/download/1.0.0/snapzap-1.0.0-macos-arm64.tar.gz"
-      sha256 "016d773a1b27bdfd3674fc4e35ffb7a0f00af1f26232ca7a1e3527d2c3ca87af"
+      sha256 "765016159902f24894a8b4151fcd8dd00c746584a41fd6fa105c6023ce6ba9b2"
     end
   end
 
