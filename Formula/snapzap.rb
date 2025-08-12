81309c251f4f5ab5f9c51eadf000357cd57f3a14
Michael Sims <sims.mike@gmail.com>
2025-08-12 01:13:08 -0700

    initial commit with snapzap

--- Formula/snapzap.rb
+++ Formula/snapzap.rb
@@ -5,12 +5,12 @@ class Snapzap < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/EasyG0ing1/SnapZap/releases/download/1.0.5/snapzap-1.0.0-macos-x64.tar.gz"
+      url "https://github.com/EasyG0ing1/SnapZap/releases/download/v1.0.5/snapzap-1.0.5-macos-x64.tar.gz"
       sha256 "22b3a8d4fdb0af6c377fe1c76017aac14c9d0672e8123fcdf67dea690a1f7214"
     end
 
     on_arm do
-      url "https://github.com/EasyG0ing1/SnapZap/releases/download/1.0.5/snapzap-1.0.0-macos-arm64.tar.gz"
+      url "https://github.com/EasyG0ing1/SnapZap/releases/download/v1.0.5/snapzap-1.0.5-macos-arm64.tar.gz"
       sha256 "765016159902f24894a8b4151fcd8dd00c746584a41fd6fa105c6023ce6ba9b2"
     end
   end
