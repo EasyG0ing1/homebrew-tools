b62e77a276a796952daa13bb22a178ac0c6d604d
runner <runner@Mac-1755071586379.local>
2025-08-13 08:03:14 +0000

    Bump snapzap to 1.0.15

--- Formula/snapzap.rb
+++ Formula/snapzap.rb
@@ -1,17 +1,17 @@
 class Snapzap < Formula
   desc "SnapZap helps you clean up snapshots on APFS volumes in MacOS"
   homepage "https://github.com/EasyG0ing1/SnapZap"
-  version "1.0.10"
+  version "1.0.15"
 
   on_macos do
     on_intel do
-      url "https://github.com/EasyG0ing1/SnapZap/releases/download/1.0.10/snapzap-1.0.10-macos-x64.tar.gz"
-      sha256 "2c6df41da87529d695f2a83a3fc7277fc02bf78d687f4b67cfc0b20f8799da4d"
+      url "https://github.com/EasyG0ing1/SnapZap/releases/download/1.0.15/snapzap-1.0.15-macos-x64.tar.gz"
+      sha256 "f2d0dd84094632e0dd60b9cad1443901498d04510941d830ddc04637363aaf14"
     end
 
     on_arm do
-      url "https://github.com/EasyG0ing1/SnapZap/releases/download/1.0.10/snapzap-1.0.10-macos-x64.tar.gz"
-      sha256 "80886466fd77526f06fd213d907dc0963cf418d6549c76abd1ad7cd606b3ba1e"
+      url "https://github.com/EasyG0ing1/SnapZap/releases/download/1.0.15/snapzap-1.0.15-macos-x64.tar.gz"
+      sha256 "bd36895732e20efa80b385e549e080220460bae9bdf6adf261e96ebc685bf5e8"
     end
   end
 
